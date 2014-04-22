d4b1834b458b26cc6e9d418b6e9fd301132770cd
Abigail Watson <awatson1978@gmail.com>
2014-04-21 23:40:57 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "Semantic syntax using Bootstrap.",
+  internal: true
+});
+
+Package.on_use(function (api) {
+  api.use(['bootstrap-3', 'less'], 'client');
+
+  api.add_files('bootstrap-semantic-syntax.less', 'client');
+});
